23ff97c45234620bb99013945f223dec456c6f1d
Andresgarciasch <andres_28garcia@outlook.es>
2022-07-22 11:39:26 +0000

    first commit

---
+++ src/js/component/footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../styles/footer.css"
+
+
+
+const Footer = () => {
+    return(
+        <>
+    <footer className="futer mt-6 d-flex py-3 text-center">
+    <div className="d-flex justify-content-center">
+      <div className="one mr-2 d-flex flex-column">
+        <h6>Copyright @ Your Website 2021</h6>
+      </div>
+
+    </div>
+  </footer>
+        </>
+    )
+}
+
+export default Footer;
